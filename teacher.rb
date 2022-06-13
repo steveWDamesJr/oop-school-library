2f9925b4918bd13236a17a6eb18762d90f5c6d0a
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-06-13 05:21:13 -0500

    Add teacher class to oop-school-library
    -Create teacher.rb file

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+require_relative './person'
+
+class Teacher < Person
+  def initialize(age:, name: 'unknown', parent_permission: true)
+    super(name: name, age: age, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
