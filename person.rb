4a573bca414d9989c47eff84b25803ca9bd831a2
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-06-13 07:50:21 -0500

    Update Person class
    -Position getters and setters before initialize method
    -Position public access methods before other methods that require access to them

--- person.rb
+++ person.rb
@@ -1,4 +1,7 @@
 class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
   def initialize(age:, name: 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
@@ -6,18 +9,13 @@ class Person
     @parent_permission = parent_permission
   end
 
-  attr_accessor :name, :age
-  attr_reader :id
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 
   private
 
   def of_age?
     @age >= 18
-  end
-
-  public
-
-  def can_use_services?
-    of_age? || @parent_permission
   end
 end
