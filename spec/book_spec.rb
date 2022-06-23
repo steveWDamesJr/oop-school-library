a8ffd807d050741f22d403f3ea11e37fd92bdbd2
levy002 <levy.ukwi002@gmail.com>
2022-06-23 19:04:41 +0200

    Add units for book class methods

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -7,4 +7,14 @@ describe Book do
     expect(@new_book.title).to eq('The Great Book')
     expect(@new_book.author).to eq('The Awesome Author')
   end
+
+  it 'Return a book rental via add_rental method' do
+    book = Book.new('Great Book', 'Great Author')
+    person = Person.new(22, 'Levy', paren t_permission: false)
+    rental = Rental.new('2022/06/16', person, book)
+
+    person.add_rental(rental)
+
+    expect(book.rentals).to eql [rental]
+  end
 end
